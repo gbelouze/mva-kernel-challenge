925b542b74a475bf22ae7b356f4c8a1c27da5dad
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2022-04-07 11:16:59 +0200

    cifar labels

--- src/challenge/visualization/visualize.py
+++ src/challenge/visualization/visualize.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt  # type: ignore
 import numpy as np
+
 from challenge.data.io import imreshape
 
 
@@ -9,3 +10,17 @@ def plot(row: np.ndarray, ax=None):
     if ax is not None:
         plt.sca(ax)
     plt.imshow(im)
+
+
+labels = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck",
+}
