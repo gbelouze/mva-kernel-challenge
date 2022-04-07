af01368dec2f53a7e9d86babdb888a53ff25759c
Gabriel Belouze <gabriel.belouze@ens.psl.eu>
2022-04-07 11:15:48 +0200

    add Polynomial add Chi2 kernels

--- src/challenge/models/kernel.py
+++ src/challenge/models/kernel.py
@@ -93,5 +93,84 @@ class Linear(Kernel):
             ret = X @ Y
         else:
             ret = np.einsum("...id,...jd->...ij", X, Y)
-        log.debug(f"Computed Gram matrix of Linear Kernel of size {ret.size}", extra={"markup": True})
+        shape = f"{ret.shape[0]}" if ret.ndim == 1 else f"{ret.shape[-2]}x{ret.shape[-1]}"
+        log.debug(f"Computed Gram matrix of Linear Kernel of size [bold cyan]{shape}[/]", extra={"markup": True})
+        return ret
+
+
+class Polynomial(Kernel):
+    r"""The polynomial kernel of degree :math:`\gamma` is defined as
+    .. math:: k(x, y) = \langle x, y \rangle^{\gamma}
+    """
+
+    def __init__(self, degree: int = 3):
+        """
+        Parameters
+        ----------
+            degree
+                The polynomial degree of the kernel
+        """
+        self.degree = degree
+
+    @overrides(check_signature=False)
+    def gram(self, X, Y):
+        X = np.array(X)
+        Y = np.array(Y)
+
+        if X.ndim == 1 and Y.ndim == 1:
+            ret = X * Y
+        elif X.ndim == 1:
+            ret = Y @ X
+        elif Y.ndim == 1:
+            ret = X @ Y
+        else:
+            ret = np.einsum("...id,...jd->...ij", X, Y)
+        ret = ret**self.degree
+
+        shape = f"{ret.shape[0]}" if ret.ndim == 1 else f"{ret.shape[-2]}x{ret.shape[-1]}"
+        log.debug(f"Computed Gram matrix of Polynomial Kernel of size [bold cyan]{shape}[/]", extra={"markup": True})
+        return ret
+
+
+class Chi2(Kernel):
+    r"""chi-squared kernel implementation.
+
+    The chi-squared kernel is defined on non negative data such that
+    .. math:: k(x, y) = \exp{-\gamma \sum_i \frac{(x_i-y_i)}^2{x_i + y_i}}
+    """
+
+    def __init__(self, gamma: float = 1.0):
+        """
+        Parameters
+        ----------
+        sigma
+            The variance of the kernel
+        """
+        self.gamma = gamma
+
+    @overrides(check_signature=False)
+    def gram(self, X, Y):
+        X = np.array(X)
+        Y = np.array(Y)
+
+        assert (X >= 0).all(), "data must be non negative"
+        assert (Y >= 0).all(), "data must be non negative"
+
+        if X.ndim == 1 and Y.ndim == 1:
+            mantiss = np.sum((X - Y) ** 2 / (X + Y))
+        elif X.ndim == 1:
+            mantiss = (X[None, :] - Y) ** 2 / (X[None, :] + Y)
+            np.nan_to_num(mantiss, copy=False)  # replace 0/0 with 0
+            mantiss = mantiss.sum(axis=1)
+        elif Y.ndim == 1:
+            mantiss = (X - Y[None, :]) ** 2 / (X + Y[None, :])
+            np.nan_to_num(mantiss, copy=False)  # replace 0/0 with 0
+            mantiss = mantiss.sum(axis=1)
+        else:
+            mantiss = (X[..., :, None, :] - Y[..., None, :, :]) ** 2 / (X[..., :, None, :] + Y[..., None, :, :])
+            np.nan_to_num(mantiss, copy=False)  # replace 0/0 with 0
+            mantiss = mantiss.sum(axis=-1)
+        ret = np.exp(-self.gamma * mantiss)
+        shape = f"{ret.shape[0]}" if ret.ndim == 1 else f"{ret.shape[-2]}x{ret.shape[-1]}"
+        log.debug(f"Computed Gram matrix for Chi2 Kernel of size [bold cyan]{shape}[/]", extra={"markup": True})
         return ret
